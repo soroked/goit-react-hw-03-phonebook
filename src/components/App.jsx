1c74cd94ecabea08f6c07c805c0f51bdc15fc68f
Eduard <sorokolietov@gmail.com>
2023-10-27 13:44:00 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,41 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
+// Data to test the App
+
+// const contacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
+
 export class App extends Component {
   state = {
     contacts: [],
     filter: '',
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    const lsContacts = JSON.parse(localStorage.getItem('contacts')) ?? [];
+
+    if (lsContacts.length) {
+      this.setState({ contacts: lsContacts });
+    }
+  }
+
+  componentDidUpdate(_, { contacts }) {
+    if (this.state.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+
+    const lsContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (!lsContacts.length) {
+      console.log('asd');
+      localStorage.removeItem('contacts');
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ [e.currentTarget.name]: e.currentTarget.value });
@@ -37,7 +65,6 @@ export class App extends Component {
   };
 
   deleteHandler = contactId => {
-    // const deleted = this.state.contacts.filter(({ id }) => id !== contactId);
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(({ id }) => id !== contactId),
     }));
